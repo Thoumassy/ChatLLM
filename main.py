6deef9b14b57bd13052803994ca248da89755ace
Thomas <t.meimoun@gmail.com>
2023-07-01 13:51:53 -0400

    updated code for pinecone

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
+import pinecone 
 from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENV = os.getenv('PINECONE_ENV')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 loader = DirectoryLoader(
     'Data/', # my local directory
@@ -6,5 +14,5 @@ loader = DirectoryLoader(
     show_progress=True
 )
 docs = loader.load()
-docs
+
 
