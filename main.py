56b7496d4be90d0a2af42a8da56bb7629ae9c313
Thomas <t.meimoun@gmail.com>
2023-07-01 14:43:59 -0400

    adding Vector Database part in the ntbk, some enhancement to provide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
 import pinecone 
-from langchain.document_loaders import DirectoryLoader
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
 
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
